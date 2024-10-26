334785674462e2d5ddeb6421ee9a8f6900f77a3b
KKT <kimgt2828@gmail.com>
2024-10-27 01:39:03 +0900

    refactor: 롬복 사용 전으로 롤백 - DTO 롬복 제거 후 수동 구현 - 엔티티 롬복 제거 후 수동 구현

--- src/main/java/com/example/board/dto/ArticleDto.java
+++ src/main/java/com/example/board/dto/ArticleDto.java
@@ -7,15 +7,54 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 //롬복을 통한 코드 리팩토링
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@ToString()
+
 public class ArticleDto {
     private Long id;
     private String title;
     private String content;
     public Article toEntity() {
         return new Article(id, title, content);
+    }
+
+    //AllArgsConstructor
+    public ArticleDto(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+
+
+    //NoArgsConstructor
+    public  ArticleDto() {
+    }
+
+    //Getters
+    public Long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+
+    //Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return "ArticleDto{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
     }
 }
--- src/main/java/com/example/board/entity/Article.java
+++ src/main/java/com/example/board/entity/Article.java
@@ -9,10 +9,6 @@ import java.util.List;
 
 
 @Entity //DB가 해당 객체를 인식가능!
-@RequiredArgsConstructor
-@AllArgsConstructor
-@ToString
-@Getter
 public class Article {
     @Id //대표값을 지정! Like a 주민번호
     @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 id를 자동 생성
@@ -26,5 +22,51 @@ public class Article {
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    //AllArgsconstructor
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+    //NoArgsConstructor
+    public Article() {
+    }
+
+    //Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    //Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
     }
 }
